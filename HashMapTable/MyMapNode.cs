976f168f518cda41cc800a7a581a27cf5a4649fd
sahithi <sahithipari99@gmail.com>
2021-07-11 22:20:13 +0530

    [sahithi] CheckingFrequency

---
+++ HashMapTable/MyMapNode.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashMapTable
+{
+    public class MyMapNode<K, V>
+    {
+        public struct KeyValue<k, v>
+        {
+            public k Key { get; set; }
+            public v Value { get; set; }
+        }
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+
+        //To get the position
+        protected int GetArrayPosition(K key)
+        {
+            int hash = key.GetHashCode();
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+
+
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+
+        //To add elements
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>()
+            { Key = key, Value = value };
+            linkedList.AddLast(item);
+            Console.WriteLine(item.Key + " " + item.Value);
+        }
+    }
+}
--- HashMapTable/Program.cs
+++ HashMapTable/Program.cs
@@ -7,6 +7,13 @@ namespace HashMapTable
         static void Main(string[] args)
         {
             Console.WriteLine("---------------------Hash Table-----------------------");
+            MyMapNode<int, string> hashTable = new MyMapNode<int, string>(6);
+            hashTable.Add(0, "To");
+            hashTable.Add(1, "be");
+            hashTable.Add(2, "or");
+            hashTable.Add(3, "not");
+            hashTable.Add(4, "to");
+            hashTable.Add(5, "be");
         }
     }
 }
